3c068a4cc41f7ae8a77022b08fdda74eae6e21f0
Shawn Yates <sdy0002@auburn.edu>
2022-04-04 14:09:37 -0700

    importing matplotlib

--- cell_death.py
+++ cell_death.py
@@ -1,3 +1,18 @@
 #! /usr/bin/env python3
 
 import sys
+import pandas as pd
+import openpyxl
+from matplotlib import pyplot as plt
+
+dataset = pd.read_excel('cell_death.xlsx')
+print(dataset)
+
+#def cell_death():
+#    print('Opening cell_death.xlsx ')
+#    with open('cell_death.xlsx ', 'r') as in_stream:
+#            print('Opening output.txt')
+#    pass
+
+#if __name__ == '__main__':
+    #cell_death()
--- titer.py
+++ titer.py
@@ -3,6 +3,7 @@
 import sys
 import pandas as pd
 import openpyxl
+from matplotlib import pyplot as plt
 
 dataset = pd.read_excel('titer.xlsx')
 print(dataset)
