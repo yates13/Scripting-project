61f7632d5f40a3d34b7b96f75e245569c185cca8
Shawn Yates <sdy0002@auburn.edu>
2022-04-15 08:17:44 -0700

    trying to figure out the next step now

--- titer.py
+++ titer.py
@@ -2,11 +2,30 @@
 
 import sys
 import pandas as pd
-import openpyxl
+import csv
+import re
 import matplotlib.pyplot as plt  
 
-wb = pd.read_excel(sys.argv[1])
-print(wb)
+df = pd.read_csv(sys.argv[1])
+#print(df)
+
+
+sample_range = df.iloc[0:150, 1]
+#print(sample_range)
+
+count = 0
+iter = 49
+virus = {}
+for i in sample_range:
+    j = re.match('12', str(i))
+    iter = iter + 1
+    if j != None:
+        count = count + 1
+        sample_means.update({wb.iloc[iter, 1]:wb.iloc[iter,6]})
+print(sample_means)
+
+
+raw_val = input("Which samples need to be normalized? Separate by a comma (eg. SPL2, SPL3, SPL4): \n")
 
 
 
