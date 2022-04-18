ee769abdb9d024c91a2295767cd1f49da433bc44
Shawn Yates <sdy0002@auburn.edu>
2022-04-18 11:55:33 -0700

    new stuff

--- titer.py
+++ titer.py
@@ -7,34 +7,58 @@ import re
 import matplotlib.pyplot as plt  
 
 df = pd.read_csv(sys.argv[1])
-#print(df)
+print(df)
 
 
-sample_range = df.iloc[0:50, 0]
-#print(sample_range)
+
+#df_name = pd.DataFrame([1,2,3])
+#df_name[0].plot(kind='bar')
+
+#plt.show()
+#Virus = df.iloc[0:50, 0]
+#print(Virus)
+
+#hour = df.iloc[0:50, 2]
+#print(hour)
+
+#log_pfu = df.iloc[0:50,1]
+#print(log_pfu)
+
+#plt.plot(hour)
+#plt.plot(log_pfu)
+#plt.plot(Virus, color = 'green')
+df.plot.line()
+plt.title('Viral Growth Curve')
+plt.xlabel('Hours')
+plt.ylabel('Log_PFU')
+plt.legend(loc='upper left')
+plt.show()
+
+
+
 
 # need to make a dictonary that time all the time point and values related to the different
 # types of viruses ( can probably have that be an input like the other scripts 
 
-count = 0
-iter = 0
-virus = {}
-for i in sample_range:
-    j = re.match('\w', str(i))
-    iter = iter + 1
-    if j != None:
-        count = count + 1
-        virus.update({df.iloc[iter, 0]:df.iloc[iter,1]})
-    else:
-        quit()
-print(virus)
+#count = 0
+#iter = 0
+#virus = {}
+#for i in sample_range:
+#    j = re.match('\w', str(i))
+#    iter = iter + 1
+ #   if j != None:
+  #      count = count + 1
+   #     virus.update({df.iloc[iter, 0]:df.iloc[iter,1]})
+   # else:
+    #    quit()
+#print(virus)
 
-counts = []
-frequencies = []
+#counts = []
+#frequencies = []
 
-for d in csv.DictReader(open('yourfile.csv'), delimiter='\t'):
-    counts.append(int(d['Counts']))
-    frequencies.append(int(d['frequency']))
+#for d in csv.DictReader(open('yourfile.csv'), delimiter='\t'):
+ #   counts.append(int(d['Counts']))
+  #  frequencies.append(int(d['frequency']))
 
 
 #raw_val = input("Which samples need to be normalized? Separate by a comma (eg. SPL2, SPL3, SPL4): \n")
