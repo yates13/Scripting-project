a413fda21c12ae3fd2a7be3ec008ed67dff7bb4a
Shawn Yates <sdy0002@auburn.edu>
2022-04-22 12:30:38 -0700

    making it modular

--- cell_death.py
+++ cell_death.py
@@ -10,50 +10,51 @@ from openpyxl.chart import BarChart,Reference
 import matplotlib.pyplot as plt
 import numpy as np
 
-#read in excel document from command line argument
-wb = pd.read_excel(sys.argv[1])
 
-#set range to search for sample list
-sample_range = wb.iloc[50:300, 1]
 
-#initialize sample count (count), line iterator (iter), sample mean dictionary (sample_means), and sample names dictionary (sample_names)
-count = 0
-iter = 49
-sample_means = {}
-sample_names = {}
-
+def sample(count, iter, sample_means, sample_names):
 #Searching for samples in sample range using regex
-for i in sample_range:
-    j = re.match('SPL', str(i))
-    iter = iter + 1
+    for i in sample_range:
+        j = re.match('SPL', str(i))
+        iter = iter + 1
     #Adding sample name and mean to sample means dict if match found
-    if j != None:
-        count = count + 1
-        sample_means.update({wb.iloc[iter, 1]:wb.iloc[iter,6]})
-        sample_names.update({wb.iloc[iter, 1]:wb.iloc[iter,2]})
+        if j != None:
+            count = count + 1
+            sample_means.update({wb.iloc[iter, 1]:wb.iloc[iter,6]})
+            sample_names.update({wb.iloc[iter, 1]:wb.iloc[iter,2]})
 
-#Prompting user for normalization
-raw_val = {}
-raw_val = input("Which samples need to be normalized? Separate by a comma (eg. SPL2, SPL3, SPL4): \n")
-raw_val_list = raw_val.split(", ")
-
-#initialize adjusted mean list for vectors
-norm_means = {}
-
-#Prompting user for control sample for each previous entry
-for i in raw_val_list:
-    norm = input("Which sample should be used to normalize " + i + "?\n")
-    adj_mean = (sample_means[i]/sample_means[norm])*100
-    tag = sample_names[i]
-    print(tag)
-    norm_means.update({tag:adj_mean})
-
-mean_values = norm_means.values()
-sample = norm_means.keys()
-
-p1 = plt.bar(sample, mean_values)
-plt.title('Cell Death Assay')
-plt.ylabel('Mean Luminescence (Normalized)')
-plt.show()
+    #Prompting user for normalization
+    raw_val = input("Which samples need to be normalized? Separate by a comma (eg. SPL2, SPL3, SPL4): \n")
+    raw_val_list = raw_val.split(", ")
 
 
+    for i in raw_val_list:
+        norm = input("Which sample should be used to normalize " + i + "?\n")
+        adj_mean = (sample_means[i]/sample_means[norm])*100
+        tag = sample_names[i]
+        print(tag)
+        norm_means.update({tag:adj_mean})
+
+
+def plotting(sample, mean_values):
+    p1 = plt.bar(sample, mean_values)
+    plt.title('Cell Death Assay')
+    plt.ylabel('Mean Luminescence (Normalized)')
+    plt.show()
+
+if __name__ == '__main__':
+    #initialize sample count (count), line iterator (iter), sample mean dictionary (sample_means), and sample names dictionary (sample_names)
+    count = 0
+    iter = 49
+    raw_val = {}
+    norm_means = {}
+    #read in excel document from command line argument
+    wb = pd.read_excel(sys.argv[1])
+    #set range to search for sample list
+    sample_range = wb.iloc[50:300, 1]
+    sample_means = {}
+    sample_names = {}
+    sample(count, iter, sample_means, sample_names)
+    mean_values = norm_means.values()
+    sample = norm_means.keys()
+    plotting(sample, mean_values)
